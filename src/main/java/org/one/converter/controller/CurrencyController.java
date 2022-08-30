d43875d88e69d3272ff8ffbe82615f3d0406d7de
Franco Lorca <lorcafrancog@gmail.com>
2022-08-30 17:53:08 -0300

    update: added error message

--- src/main/java/org/one/converter/controller/CurrencyController.java
+++ src/main/java/org/one/converter/controller/CurrencyController.java
@@ -8,6 +8,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
 import org.one.converter.model.request.CurrencyRequest;
 import org.one.converter.model.response.CurrencyResponse;
 import org.one.converter.service.CurrencyService;
@@ -33,16 +34,23 @@ public class CurrencyController implements Initializable {
   }
 
   public void currencyConvert() {
-    CurrencyRequest currencyRequest = new CurrencyRequest();
 
-    currencyRequest.setUrl("https://api.exchangerate.host/convert");
-    currencyRequest.setFrom(fromCurrency.getValue());
-    currencyRequest.setTo(toCurrency.getValue());
-    currencyRequest.setAmount(Double.parseDouble(textField.getText()));
+    if(fromCurrency.getValue() != null && toCurrency.getValue() != null && !textField.getText().isEmpty()) {
 
-    CurrencyResponse result = currencyService.get(currencyRequest);
+      CurrencyRequest currencyRequest = new CurrencyRequest();
 
-    output.setText(String.format("%s %s", result.getAmount(), result.getCode().substring(1,4)));
+      currencyRequest.setUrl("https://api.exchangerate.host/convert");
+      currencyRequest.setFrom(fromCurrency.getValue());
+      currencyRequest.setTo(toCurrency.getValue());
+      currencyRequest.setAmount(Double.parseDouble(textField.getText()));
+
+      CurrencyResponse result = currencyService.get(currencyRequest);
+
+      output.setText(String.format("%s %s", result.getAmount(), result.getCode().substring(1,4)));
+    } else {
+      output.setText("You have to provide amount and currencies");
+      output.setTextFill(Color.RED);
+    }
   }
 
   @Override
